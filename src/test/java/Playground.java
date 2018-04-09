02d2423eb61a87df63a7b9fe587e97560886508a
Simon Rehwald <simon.rehwald@tum.de>
2018-04-09 19:22:35 +0200

    add code snippet

---
+++ src/test/java/Playground.java
@@ -0,0 +1,39 @@
+import org.junit.Ignore;
+import org.junit.Test;
+import org.logicng.datastructures.Assignment;
+import org.logicng.datastructures.Tristate;
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+import org.logicng.formulas.Literal;
+import org.logicng.formulas.Variable;
+import org.logicng.solvers.MiniSat;
+import org.logicng.solvers.SATSolver;
+
+public class Playground {
+    @Ignore
+    @Test
+    public void snippet() {
+        FormulaFactory f = new FormulaFactory();
+        Variable a = f.variable("A");
+        Variable b = f.variable("B");
+        Literal notC = f.literal("C", false);
+
+        // A & ~(B | ~C); CNF: A & ~B & C => true for A=1, B=0, C=1
+        Formula formula = f.and(a, f.not(f.or(b, notC)));
+        // assign a positive literal for TRUE and a negative literal for FALSE
+        Assignment assignment = new Assignment();
+        assignment.addLiteral(a);
+        assignment.addLiteral(b.negate());
+        assignment.addLiteral(notC.negate());
+        System.out.println(formula.evaluate(assignment)); // true
+
+        // NNF and CNF
+        Formula nnf = formula.nnf();
+        Formula cnf = formula.cnf();
+
+        // SAT
+        final SATSolver miniSat = MiniSat.miniSat(f);
+        miniSat.add(formula);
+        final Tristate result = miniSat.sat();
+    }
+}
