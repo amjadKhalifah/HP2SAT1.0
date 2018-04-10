e4be22addb379cc811f3c7be2647f093af7638a0
Simon Rehwald <simon.rehwald@tum.de>
2018-04-10 12:19:43 +0200

    add exogenous variables

--- src/test/java/de/in/tum/i4/hp2sat/modeling/CausalModelTest.java
+++ src/test/java/de/in/tum/i4/hp2sat/modeling/CausalModelTest.java
@@ -1,5 +1,6 @@
 package de.in.tum.i4.hp2sat.modeling;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.logicng.formulas.Formula;
 import org.logicng.formulas.FormulaFactory;
@@ -12,10 +13,15 @@ import java.util.Set;
 import static org.junit.Assert.*;
 
 public class CausalModelTest {
+    FormulaFactory f;
+
+    @Before
+    public void setUp() throws Exception {
+        f = new FormulaFactory();
+    }
+
     @Test
     public void Should_CreateCausalModel_When_EverythingFine() {
-        FormulaFactory f = new FormulaFactory();
-
         Variable BTExo = f.variable("BT_exo");
         Variable STExo = f.variable("ST_exo");
 
@@ -31,15 +37,18 @@ public class CausalModelTest {
         Formula BHFormula = f.and(BT, f.not(SH));
         Formula BSFormula = f.and(ST, BT);
 
+        ExogenousEquation BTExoEquation = new ExogenousEquation(BTExo);
+        ExogenousEquation STExoEquation = new ExogenousEquation(STExo);
+
         EndogenousEquation BTEquation = new EndogenousEquation(BT, BTFormula);
         EndogenousEquation STEquation = new EndogenousEquation(ST, STFormula);
         EndogenousEquation SHEquation = new EndogenousEquation(SH, SHFormula);
         EndogenousEquation BHEquation = new EndogenousEquation(BH, BHFormula);
         EndogenousEquation BSEquation = new EndogenousEquation(BS, BSFormula);
 
-        Set<Equation> equations =
-                new HashSet<>(Arrays.asList(BTEquation, STEquation, SHEquation, BHEquation, BSEquation));
-        
+        Set<Equation> equations = new HashSet<>(Arrays.asList(BTEquation, STEquation, SHEquation, BHEquation,
+                BSEquation, BTExoEquation, STExoEquation));
+
         CausalModel causalModel = new CausalModel("BillySuzy", equations);
     }
 }
