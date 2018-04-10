412da61efd627f075042e5ae1ad8aeb61451b96b
Simon Rehwald <simon.rehwald@tum.de>
2018-04-10 16:54:35 +0200

    move billy suzy example into ExampleProvider class

--- src/test/java/de/in/tum/i4/hp2sat/modeling/CausalModelTest.java
+++ src/test/java/de/in/tum/i4/hp2sat/modeling/CausalModelTest.java
@@ -1,6 +1,7 @@
 package de.in.tum.i4.hp2sat.modeling;
 
 import de.in.tum.i4.hp2sat.exceptions.InvalidCausalModelException;
+import de.in.tum.i4.hp2sat.testutil.ExampleProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.logicng.formulas.Formula;
@@ -24,32 +25,7 @@ public class CausalModelTest {
 
     @Test
     public void Should_CreateCausalModel_When_EverythingFine() throws InvalidCausalModelException {
-        Variable BTExo = f.variable("BT_exo");
-        Variable STExo = f.variable("ST_exo");
-
-        Variable BT = f.variable("BT");
-        Variable ST = f.variable("ST");
-        Variable BH = f.variable("BH");
-        Variable SH = f.variable("SH");
-        Variable BS = f.variable("BS");
-
-        Formula BTFormula = BTExo;
-        Formula STFormula = STExo;
-        Formula SHFormula = ST;
-        Formula BHFormula = f.and(BT, f.not(SH));
-        Formula BSFormula = f.and(ST, BT);
-
-        Equation BTEquation = new Equation(BT, BTFormula);
-        Equation STEquation = new Equation(ST, STFormula);
-        Equation SHEquation = new Equation(SH, SHFormula);
-        Equation BHEquation = new Equation(BH, BHFormula);
-        Equation BSEquation = new Equation(BS, BSFormula);
-
-        Set<Equation> equations = new HashSet<>(Arrays.asList(BTEquation, STEquation, SHEquation, BHEquation,
-                BSEquation));
-        Set<Variable> exogenousVariables = new HashSet<>(Arrays.asList(BTExo, STExo));
-
-        CausalModel causalModel = new CausalModel("BillySuzy", equations, exogenousVariables);
+        ExampleProvider.billySuzy();
     }
 
     @Test
