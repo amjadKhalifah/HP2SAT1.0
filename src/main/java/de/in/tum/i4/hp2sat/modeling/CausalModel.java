5a7da6bdc82f461be37bd475c4e414ee73965c41
Simon Rehwald <simon.rehwald@tum.de>
2018-04-10 11:33:53 +0200

    split equations into endo and exo

--- src/main/java/de/in/tum/i4/hp2sat/modeling/CausalModel.java
+++ src/main/java/de/in/tum/i4/hp2sat/modeling/CausalModel.java
@@ -1,14 +1,25 @@
 package de.in.tum.i4.hp2sat.modeling;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CausalModel {
     private String name;
     private Set<Equation> equations;
 
+    private Set<EndogenousEquation> endogenousEquations;
+    private Set<ExogenousEquation> exogenousEquations;
+
     public CausalModel(String name, Set<Equation> equations) {
         this.name = name;
         this.equations = equations;
+
+        this.endogenousEquations = this.equations.stream()
+                .filter(e -> e instanceof EndogenousEquation)
+                .map(e -> (EndogenousEquation) e).collect(Collectors.toSet());
+        this.exogenousEquations = this.equations.stream()
+                .filter(e -> e instanceof ExogenousEquation)
+                .map(e -> (ExogenousEquation) e).collect(Collectors.toSet());
     }
 
     public String getName() {
