09cd0ba4ef774fbb8fcf05688fda8bc44b27aed2
Simon Rehwald <simon.rehwald@tum.de>
2018-04-28 11:04:51 +0200

    implement copy constructors

--- src/main/java/de/tum/in/i4/hp2sat/causality/Equation.java
+++ src/main/java/de/tum/in/i4/hp2sat/causality/Equation.java
@@ -12,15 +12,28 @@ public class Equation {
         this.formula = formula;
     }
 
+    public Equation(Equation equation) {
+        this(equation.variable, equation.formula);
+    }
+
     @Override
     public String toString() {
         return variable + " = " + formula;
     }
+
     public Variable getVariable() {
         return variable;
     }
 
     public Formula getFormula() {
         return formula;
+    }
+
+    public void setVariable(Variable variable) {
+        this.variable = variable;
+    }
+
+    public void setFormula(Formula formula) {
+        this.formula = formula;
     }
 }
