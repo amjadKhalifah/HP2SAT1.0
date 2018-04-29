b3be7374e087740346e79226d2deae19a71a6393
Simon Rehwald <simon.rehwald@tum.de>
2018-04-29 15:18:29 +0200

    copy test cases

--- src/test/java/de/tum/in/i4/hp2sat/causality/SATCausalitySolverTest.java
+++ src/test/java/de/tum/in/i4/hp2sat/causality/SATCausalitySolverTest.java
@@ -72,4 +72,125 @@ public class SATCausalitySolverTest {
                 new HashSet<>(Arrays.asList(f.variable("BS"), f.literal("BH", false)))),
                 causalitySolverResult);
     }
+
+    @Test
+    public void Should_FulfillAllACs_When_SHIsCauseBS() throws Exception {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("BT_exo", true), f.literal("ST_exo", true)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("SH")));
+        Formula phi = f.variable("BS");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(billySuzy, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, true, true, cause,
+                new HashSet<>(Collections.singletonList(f.literal("BH", false)))), causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAllACs_When_NotBTIsCauseForNotBS() throws Exception {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("BT_exo", false), f.literal("ST_exo", false)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.literal("BT", false)));
+        Formula phi = f.literal("BS", false);
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(billySuzy, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, true, true, cause, new HashSet<>()),
+                causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAC1AndAC2Only_When_BTAndSTIsCauseForBS() throws Exception {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("BT_exo", true), f.literal("ST_exo", true)));
+        Set<Literal> cause = new HashSet<>(Arrays.asList(f.variable("BT"), f.variable("ST")));
+        Formula phi = f.variable("BS");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(billySuzy, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, true, false, cause, new HashSet<>()),
+                causalitySolverResult);
+    }
+
+    @Test
+    public void Should_NotFulfillACs_When_NotBTIsCauseForBS() throws Exception {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("BT_exo", true), f.literal("ST_exo", true)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.literal("BT", false)));
+        Formula phi = f.variable("BS");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(billySuzy, context, phi, cause);
+        assertEquals(new CausalitySolverResult(false, false, true, cause, null), causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAllACs_When_STIsCauseBSOrSH() throws Exception {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("BT_exo", true), f.literal("ST_exo", true)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("ST")));
+        Formula phi = f.or(f.variable("BS"), f.variable("SH"));
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(billySuzy, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, true, true, cause,
+                new HashSet<>(Collections.singletonList(f.literal("BH", false)))), causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAC1AC3Only_When_LIsCauseForFF() throws Exception {
+        CausalModel arsonists = ExampleProvider.arsonists(true);
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("L_exo", true), f.literal("MD_exo", true)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("L")));
+        Formula phi = f.variable("FF");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(arsonists, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, false, true, cause, null), causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAllAcs_When_LAndMDIsCauseForFF() throws Exception {
+        CausalModel arsonists = ExampleProvider.arsonists(true);
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("L_exo", true), f.literal("MD_exo", true)));
+        Set<Literal> cause = new HashSet<>(Arrays.asList(f.variable("L"), f.variable("MD")));
+        Formula phi = f.variable("FF");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(arsonists, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, true, true, cause, new HashSet<>()),
+                causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAllACs_When_STIsCauseForBSInExtendedModelWITHOUTWind() throws Exception {
+        CausalModel billySuzy = ExampleProvider.billySuzyExtended();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("BT_exo", true), f.literal("ST_exo", true),
+                f.literal("W_exo", false)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("ST")));
+        Formula phi = f.variable("BS");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(billySuzy, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, true, true, cause,
+                new HashSet<>(Collections.singletonList(f.literal("BH", false)))), causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAC2AC3_When_STIsCauseForBSInExtendedModelWITHWind() throws Exception {
+        CausalModel billySuzy = ExampleProvider.billySuzyExtended();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("BT_exo", true), f.literal("ST_exo", true),
+                f.literal("W_exo", true)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("ST")));
+        Formula phi = f.variable("BS");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(billySuzy, context, phi, cause);
+        assertEquals(new CausalitySolverResult(false, true, true, cause, new HashSet<>()),
+                causalitySolverResult);
+    }
+
+    @Test
+    public void Should_FulfillAllACs_When_CIsCauseForDGivenAAndNotB() throws Exception {
+        CausalModel guns = ExampleProvider.guns();
+        Set<Literal> context = new HashSet<>(Arrays.asList(
+                f.literal("A_exo", true), f.literal("B_exo", false),
+                f.literal("C_exo", true)));
+        Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("C")));
+        Formula phi = f.variable("D");
+        CausalitySolverResult causalitySolverResult = SATCausalitySolver.solve(guns, context, phi, cause);
+        assertEquals(new CausalitySolverResult(true, true, true, cause, new HashSet<>()),
+                causalitySolverResult);
+    }
 }
