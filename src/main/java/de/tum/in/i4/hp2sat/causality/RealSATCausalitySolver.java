9492d3073edd2e196ed87114e7d89418566a5190
Simon Rehwald <simon.rehwald@tum.de>
2018-05-01 13:31:49 +0200

    change sat query construction

--- src/main/java/de/tum/in/i4/hp2sat/causality/RealSATCausalitySolver.java
+++ src/main/java/de/tum/in/i4/hp2sat/causality/RealSATCausalitySolver.java
@@ -46,7 +46,7 @@ class RealSATCausalitySolver extends CausalitySolver {
             return new HashSet<>();
         }
         // generate SAT query
-        Formula formula = generateSATQuery(causalModel, phiFormula, cause, context, evaluation, f);
+        Formula formula = generateSATQuery(causalModelModified, phiFormula, cause, context, evaluation, f);
         // add query to solver
         satSolver.add(formula);
         if (satSolver.sat() == Tristate.TRUE) {
@@ -69,31 +69,34 @@ class RealSATCausalitySolver extends CausalitySolver {
     /**
      * Generates a formula whose satisfiability indicates whether AC2 is fulfilled or not.
      *
-     * @param causalModel the causal model
-     * @param notPhi      the negated phi
-     * @param cause       the cause
-     * @param context     the context
-     * @param evaluation  the original evaluation under the given context
-     * @param f           a formula factory
+     * @param causalModelModified the causal model
+     * @param notPhi              the negated phi
+     * @param cause               the cause
+     * @param context             the context
+     * @param evaluation          the original evaluation under the given context
+     * @param f                   a formula factory
      * @return a formula
      */
-    private Formula generateSATQuery(CausalModel causalModel, Formula notPhi, Set<Literal> cause, Set<Literal> context,
+    private Formula generateSATQuery(CausalModel causalModelModified, Formula notPhi, Set<Literal> cause, Set<Literal> context,
                                      Set<Literal> evaluation, FormulaFactory f) {
         // get all variables in cause
         Set<Variable> causeVariables = cause.stream().map(Literal::variable).collect(Collectors.toSet());
-        // create formula: !phi AND context AND !cause
-        Formula formula = f.and(notPhi, f.and(context), f.not(f.and(cause)));
-        for (Equation equation : causalModel.getEquations()) {
-            if (!causeVariables.contains(equation.getVariable())) {
-                // get value of variable in original iteration
-                Literal originalValue = evaluation.stream().filter(l -> l.variable().equals(equation.getVariable()))
-                        .findFirst().get(); // we know that it exists! -> no need to check isPresent()
-                // create formula: V_originalValue OR (V <=> Formula_V)
-                Formula equationFormula = f.or(originalValue,
-                        f.equivalence(equation.getVariable(), equation.getFormula()));
-                // add created formula to global formula by AND
-                formula = f.and(formula, equationFormula);
-            }
+        // create formula: !phi AND context
+        Formula formula = f.and(notPhi, f.and(context));
+        for (Equation equation : causalModelModified.getEquations()) {
+            // get value of variable in original iteration
+            Literal originalValue = evaluation.stream().filter(l -> l.variable().equals(equation.getVariable()))
+                    .findFirst().get(); // we know that it exists! -> no need to check isPresent()
+            /*
+             * create formula: V_originalValue OR (V <=> Formula_V)
+             * if the variable of the current equation is in the cause, then we do not allow for its original value
+             * and just add (V <=> Formula_V). Notice that Formula_V will be a constant if V is in the cause since we
+             * are considering the modified causal model. */
+            Formula equationFormula = causeVariables.contains(equation.getVariable()) ?
+                    f.equivalence(equation.getVariable(), equation.getFormula()) :
+                    f.or(originalValue, f.equivalence(equation.getVariable(), equation.getFormula()));
+            // add created formula to global formula by AND
+            formula = f.and(formula, equationFormula);
         }
         return formula;
     }
