c7e56bb088c3041d47c8790ccf1de0a6f948a95b
Simon Rehwald <simon.rehwald@tum.de>
2018-06-29 10:11:27 +0200

    use formula factory of causal model

--- src/test/java/de/tum/in/i4/hp2sat/causality/CausalitySolverAbstractTest.java
+++ src/test/java/de/tum/in/i4/hp2sat/causality/CausalitySolverAbstractTest.java
@@ -17,121 +17,126 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 
 public class CausalitySolverAbstractTest {
-    FormulaFactory f;
-
-    @Before
-    public void setUp() throws Exception {
-        f = new FormulaFactory();
-    }
-
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSTIsCause_BS() throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("BS");
         Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("ST")));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("BS"), f.variable("SH"),
                 f.variable("BH")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenNotSTIsCause_BS() throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("BS");
         Set<Literal> cause = new HashSet<>(Collections.singletonList(f.literal("ST", false)));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("BS"), f.variable("SH"),
                 f.variable("BH")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenBTIsCause_BS() throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("BS");
         Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("BT")));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("BS"),
                 f.variable("BH")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
-    public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSTAndBTIsCause_BS() throws InvalidCausalModelException {
+    public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSTAndBTIsCause_BS()
+            throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("BS");
         Set<Literal> cause = new HashSet<>(Arrays.asList(f.variable("ST"), f.variable("BT")));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("BS"), f.variable("SH"),
                 f.variable("BH")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSHIsCause_BS() throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("BS");
         Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("SH")));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("BS"),
                 f.variable("BH")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSHAndBHIsCause_BS()
             throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("BS");
         Set<Literal> cause = new HashSet<>(Arrays.asList(f.variable("SH"), f.variable("BH")));
         Set<Variable> wVariablesExpected = new HashSet<>(Collections.singletonList(f.variable("BS")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSTAndSHIsCause_BS()
             throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("BS");
         Set<Literal> cause = new HashSet<>(Arrays.asList(f.variable("ST"), f.variable("SH")));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("BS"),
                 f.variable("BH")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSTIsCause_SH()
             throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.variable("SH");
         Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("ST")));
         Set<Variable> wVariablesExpected = new HashSet<>(Collections.singletonList(f.variable("SH")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForBillySuzy_GivenSTIsCause_SHAndBS()
             throws InvalidCausalModelException {
+        CausalModel billySuzy = ExampleProvider.billySuzy();
+        FormulaFactory f = billySuzy.getFormulaFactory();
         Formula phi = f.and(f.variable("SH"), f.variable("BS"));
         Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("ST")));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("SH"), f.variable("BH"),
                 f.variable("BS")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.billySuzy(), phi, cause,
-                f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(billySuzy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 
     @Test
     public void Should_ReturnCorrectWVariables_ForDummy_GivenCIsCause_F() throws InvalidCausalModelException {
+        CausalModel dummy = ExampleProvider.dummy();
+        FormulaFactory f = dummy.getFormulaFactory();
         Formula phi = f.variable("F");
         Set<Literal> cause = new HashSet<>(Collections.singletonList(f.variable("C")));
         Set<Variable> wVariablesExpected = new HashSet<>(Arrays.asList(f.variable("G"), f.variable("F"),
                 f.variable("H")));
-        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(ExampleProvider.dummy(), phi, cause, f);
+        Set<Variable> wVariablesActual = CausalitySolver.getMinimalWVariables(dummy, phi, cause, f);
         assertEquals(wVariablesExpected, wVariablesActual);
     }
 }
