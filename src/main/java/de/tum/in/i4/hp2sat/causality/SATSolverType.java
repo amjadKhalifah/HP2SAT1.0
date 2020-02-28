9898a017c6481822c3b22800681cdab5769483bc
Amjad Ibrahim <>
2020-02-28 13:54:20 +0100

    create fresh copy before merge

--- src/main/java/de/tum/in/i4/hp2sat/causality/SATSolverType.java
+++
@@ -1,5 +0,0 @@
-package de.tum.in.i4.hp2sat.causality;
-
-public enum SATSolverType {
-    MINISAT, GLUCOSE, MINICARD, CLEANLING
-}
--- src/main/java/de/tum/in/i4/hp2sat/causality/SolvingStrategy.java
+++
@@ -1,6 +0,0 @@
-package de.tum.in.i4.hp2sat.causality;
-
-public enum SolvingStrategy {
-    BRUTE_FORCE, SAT, SAT_MINIMAL, SAT_COMBINED, SAT_COMBINED_MINIMAL, 
-       SAT_OPTIMIZED_AC3,  SAT_OPTIMIZED_AC3_MINIMAL
-}
